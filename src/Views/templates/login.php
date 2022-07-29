51caa7e0930b1a2506eb783cd49c98b6a2b03174
pedromoreira49 <jpedrostonemoreira@gmail.com>
2022-07-28 22:47:15 -0300

    add login file

---
+++ src/Views/templates/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>src/views/templates/css/styles.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>New Daw</title>
+</head>
+<body>
+<div class="form-style-6">
+	<h1>Login:</h1>
+	<form method="post">
+		<input type="text" name="email" placeholder="Email" />
+		<input type="password" name="pass" placeholder="Senha" />
+		<input type="submit" value="Entrar" />
+		<input type="hidden" name="login">
+	</form>
+	<input type="button" value="Registrar-se" onclick="">
+</div>
+</body>
+</html>
