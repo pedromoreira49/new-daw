bf0933ccf5751ae31f4c2c6813655673d40ff4e9
pedromoreira49 <jpedrostonemoreira@gmail.com>
2022-07-28 22:22:46 -0300

    Add Controller file of home page

---
+++ src/Controllers/HomeController.php
@@ -0,0 +1,44 @@
+<?php
+
+	namespace src\Controllers;
+
+	class HomeController{
+		public function index(){
+
+			if(isset($_GET['loggout'])){
+				session_unset();
+				session_destroy();
+
+				\src\Utils::redirect(INCLUDE_PATH);	
+			}
+
+
+			if(isset($_SESSION['login'])){
+				\src\Views\MainView::render('home');
+			}else{
+				if(isset($_POST['login'])){
+						$login = $_POST['email'];
+						$senha = $_POST['pass'];
+
+						$verifica = \src\Mysql::connect()->prepare("SELECT * FROM usuarios WHERE email = ?");
+						$verifica->execute(array($login));
+
+						if($verifica->rowCount() == 0){
+							\src\Utils::alerta('Dados incorretos! :(');
+							\src\Utils::redirect(INCLUDE_PATH);
+						}else{
+							$dados = $verifica->fetch();
+							$password = $dados['senha'];
+							$_SESSION['login'] = $dados['email'];
+							$_SESSION['id'] = $dados['idusuario'];
+							$_SESSION['nome'] = explode(' ', $dados['nome'])[0];
+							\src\Utils::alerta('Logado com sucesso!');
+							\src\Utils::redirect(INCLUDE_PATH);
+						}
+				}
+				\src\Views\MainView::render('login');
+			}
+		}
+	}
+
+?>
