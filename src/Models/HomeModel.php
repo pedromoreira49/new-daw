79812bd555a16d371b59ac21ce2a79f6451589c7
pedromoreira49 <jpedrostonemoreira@gmail.com>
2022-08-02 10:23:56 -0300

    Add primary functions for home Models

--- src/Models/HomeModel.php
+++ src/Models/HomeModel.php
@@ -2,7 +2,34 @@
 	namespace src\models;
 
 	class HomeModel{
-		
+		public static function getAllProducts(){
+			$pdo = \src\Mysql::connect();
+
+			$produtos = $pdo->prepare("SELECT * FROM produtos");
+
+			$produtos->execute();
+
+			return $produtos->fetchAll();
+		}
+
+		public static function getProductsByName($name){
+			$pdo = \src\Mysql::connect();
+
+			$produtos = $pdo->prepare("SELECT * FROM produtos WHERE nome LIKE ?");
+
+			$produtos->execute(array($name));
+		}
+
+		public static function getCategory(){
+			$pdo = \src\Mysql::connect();
+
+			$categorias = $pdo->prepare("SELECT * FROM categorias");
+
+			$categorias->execute();
+
+			return $categorias->fetchAll();
+		}
+
 	}
 
 
