0de084a272d763441b841a3e0e14904beb62f969
pedromoreira49 <jpedrostonemoreira@gmail.com>
2022-08-02 10:27:25 -0300

    Add features for get products in home

--- src/Views/templates/home.php
+++ src/Views/templates/home.php
@@ -15,12 +15,38 @@
 	<div class="w50 center">
 		<h1><a href="<?php echo INCLUDE_PATH ?>?loggout"><i class="fa fa-sign-out" aria-hidden="true"></i>Logout</a></h1>
 	</div>
-	<form action="actions/procurarProdutos.php" method="post">
-		<input type="text" name="nome" placeholder="Pesquisar produtos" />
-		<input type="submit" value="Procurar" />
-		<input type="button" value="Cadastrar Categoria" onclick="window.location.href='categorias.php'">
-		<input type="button" value="Cadastrar Produtos" onclick="window.location.href='produtos.php'">
+	<form method="post">
+		<input type="text" name="produto" placeholder="Pesquisar produtos" />
+		<input type="hidden" name="procurar" />
+		<input type="submit" name="acao" value="Procurar" />
 	</form>
 </div>
+
+<?php
+	$produtos = \src\Models\HomeModel::getAllProducts();
+	$category = \src\Models\HomeModel::getCategory();
+	$categorias = array();
+	foreach ($category as $key => $value){
+		$categorias[$key]['idcategoria'] = $value['idcategoria'];
+		$categorias[$key]['nome'] = $value['nome'];
+	}
+	foreach ($produtos as $key => $value) {
+		$produto = $produtos[$key]['idcategoria'];
+?>
+	<div class="form-style-6">
+		<div class="w50 center"><h1><?php echo $value['nome']; ?></h1></div>
+		<div class="w50 center"><h1>Quantidade: <?php echo $value['quantidade']; ?></h1></div>
+	</div>
+	<div class="card"><p><?php echo $value['descricao']; ?></p></div>
+	<div class="card"><p><?php foreach($categorias as $key => $value) {
+		if($produto == $value['idcategoria']){
+			echo $value['nome'];
+		}
+	}
+	?></p></div>
+<?php
+	}
+?>
+
 </body>
 </html>
