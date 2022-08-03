0aeffe390757c05aa7614a57f6feee17b77dfadb
pedromoreira49 <jpedrostonemoreira@gmail.com>
2022-08-03 19:35:39 -0300

    Add verify hashed password in login Controller

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -17,24 +17,29 @@
 				\src\Views\MainView::render('home');
 			}else{
 				if(isset($_POST['login'])){
-						$login = $_POST['email'];
-						$senha = $_POST['pass'];
+					$login = $_POST['email'];
+					$senha = $_POST['pass'];
 
-						$verifica = \src\Mysql::connect()->prepare("SELECT * FROM usuarios WHERE email = ?");
-						$verifica->execute(array($login));
+					$verifica = \src\Mysql::connect()->prepare("SELECT * FROM usuarios WHERE email = ?");
+					$verifica->execute(array($login));
 
-						if($verifica->rowCount() == 0){
-							\src\Utils::alerta('Dados incorretos! :(');
-							\src\Utils::redirect(INCLUDE_PATH);
-						}else{
-							$dados = $verifica->fetch();
-							$password = $dados['senha'];
+					if($verifica->rowCount() == 0){
+						\src\Utils::alerta('Email incorretos! :(');
+						\src\Utils::redirect(INCLUDE_PATH);
+					}else{
+						$dados = $verifica->fetch();
+						$password = $dados['senha'];
+						if(\src\Bcrypt::check($senha, $password)){
 							$_SESSION['login'] = $dados['email'];
 							$_SESSION['id'] = $dados['idusuario'];
 							$_SESSION['nome'] = explode(' ', $dados['nome'])[0];
 							\src\Utils::alerta('Logado com sucesso!');
 							\src\Utils::redirect(INCLUDE_PATH);
+						}else{
+							\src\Utils::alerta('Dados incorretos!');
+							\src\Utils::redirect(INCLUDE_PATH);
 						}
+					}
 				}
 				\src\Views\MainView::render('login');
 			}
