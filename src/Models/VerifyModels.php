49f72b584d8c0e87ad500d1dbe9181006f252f85
pedromoreira49 <jpedrostonemoreira@gmail.com>
2022-08-02 21:32:14 -0300

    Create simple function for gen code

---
+++ src/Models/VerifyModels.php
@@ -0,0 +1,26 @@
+<?php 
+
+	namespace src\Models;
+
+	class VerifyModel{
+
+		public static function genCode(){
+			$numbers = "0123456789";
+			srand((double)microtime()*1000000);
+
+			$i = 0;
+			$pass = '';
+			while($i <= 7){
+				$num = rand() % 33;
+				$tmp = substr($numbers, $num, 1);
+				$pass = $pass . $tmp;
+				$i++;
+			}
+
+			return $pass;
+		}
+
+	}
+
+
+?>
