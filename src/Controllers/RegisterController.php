c761d317be9a7a59802ac86d206416d4d62d8d8a
pedromoreira49 <jpedrostonemoreira@gmail.com>
2022-08-03 22:02:40 -0300

    Add logic for verify code in email

--- src/Controllers/RegisterController.php
+++ src/Controllers/RegisterController.php
@@ -11,37 +11,50 @@
 				$email = $_POST['email'];
 				$password = $_POST['pass'];
 				
-				if($_FILES['imagem']['tmp_name'] != ''){
-					$image = $_FILES['imagem'];
-					$fileExt = explode('.', $image['name']);
-					$fileExt = $fileExt[count($fileExt) -1];
-					if($fileExt == 'png' || $fileExt == 'jpg' || $fileExt == 'jpeg'){
-						$size = intval($image['size'] / 1024);
-						if($size <= 500){
-							$uniqueID = uniqid().'.'.$fileExt;
-							$sql = \src\Mysql::connect()->prepare("INSERT INTO usuarios VALUES (?, ?, ?, ?, ?, ?, ?)");
-							$sql->execute(array(null, $nome, $email, $password, $uniqueID, 1, 0));
-							move_uploaded_file($image['tmp_name'], 'c:/xampp/htdocs/new-daw/uploads/'.$uniqueID);
-							\src\Utils::alerta('Registrado com sucesso');
-							\src\Utils::redirect(INCLUDE_PATH);
+				if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+					\src\Utils::alerta('Email inválido!');
+					\src\Utils::redirect(INCLUDE_PATH.'register');
+				}else if(strlen($password) < 6){
+					\src\Utils::alerta('Senha muito curta, minimo 6 caracteres');
+					\src\Utils::redirect(INCLUDE_PATH.'register');
+				}else if(\src\Models\UsersModels::dbVerifyEmail($email)){
+					\src\Utils::alerta('Este email já esta em uso');
+					\src\Utils::redirect(INCLUDE_PATH.'register');
+				}else{
+					$password = \src\Bcrypt::hash($password);
+					if($_FILES['imagem']['tmp_name'] != ''){
+						$image = $_FILES['imagem'];
+						$fileExt = explode('.', $image['name']);
+						$fileExt = $fileExt[count($fileExt) -1];
+						if($fileExt == 'png' || $fileExt == 'jpg' || $fileExt == 'jpeg'){
+							$size = intval($image['size'] / 1024);
+							if($size <= 500){
+								$uniqueID = uniqid().'.'.$fileExt;
+								$_SESSION['code'] = \src\Models\EmailModels::sender($email, $nome);
+								$sql = \src\Mysql::connect()->prepare("INSERT INTO usuarios VALUES (?, ?, ?, ?, ?, ?, ?)");
+								$sql->execute(array(null, $nome, $email, $password, $uniqueID, 1, 1));
+								move_uploaded_file($image['tmp_name'], 'c:/xampp/htdocs/new-daw/uploads/'.$uniqueID);
+								$_SESSION['email'] = $email;
+								\src\Utils::alerta('Verifique sua caixa de email');
+								\src\Utils::redirect(INCLUDE_PATH.'verifyCode');	
+							}else{
+								\src\Utils::alerta('Tamanho da imagems muito grande!');
+								\src\Utils::redirect(INCLUDE_PATH);
+							}
 						}else{
-							\src\Utils::alerta('Tamanho da imagems muito grande!');
+							\src\Utils::alerta('Formato de imagem não permitido!');
 							\src\Utils::redirect(INCLUDE_PATH);
 						}
 					}else{
-						\src\Utils::alerta('Formato de imagem não permitido!');
-						\src\Utils::redirect(INCLUDE_PATH);
+						$_SESSION['code'] = \src\Models\EmailModels::sender($email, $nome);
+						$sql = \src\Mysql::connect()->prepare("INSERT INTO usuarios VALUES (?, ?, ?, ?, ?, ?, ?)");
+						$sql->execute(array(null, $nome, $email, $password, null, 1, 0));
+						$_SESSION['email'] = $email;
+						\src\Utils::alerta('Verifique sua caixa de email');
+						\src\Utils::redirect(INCLUDE_PATH.'verifyCode');
 					}
-				}else{
-					$sql = \src\Mysql::connect()->prepare("INSERT INTO usuarios VALUES (?, ?, ?, ?, ?, ?, ?)");
-					$sql->execute(array(null, $nome, $email, $password, null, 1, 0));
-					\src\Utils::alerta('Registrado com sucesso!');
-					\src\Utils::redirect(INCLUDE_PATH);
 				}
-
 			}
-
-
 			\src\Views\MainView::render('register');
 		}
 
