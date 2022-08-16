0dd4614683845a433e22926d0ca3b95193a214fe
pedromoreira49 <jpedrostonemoreira@gmail.com>
2022-08-16 15:42:31 -0300

    Add path to change password before verify code in forgot password

---
+++ src/Views/templates/changepassword.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>src/views/templates/css/styles.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>New Daw</title>
+</head>
+<body>
+<div class="form-style-6">
+	<h1>Informe a nova senha:</h1>
+	<form method="post">
+		<input type="password" name="newPass" placeholder="New Password..." />
+		<input type="password" name="confirm" placeholder="Confirm Password..." />
+		<input type="submit" name="acao" value="Send" />
+		<input type="hidden" name="change">
+	</form>
+</div>
+</body>
+</html>
