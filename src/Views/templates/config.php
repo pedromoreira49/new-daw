ac47b65bad6255f2ed1b940858a43883d86878ba
pedromoreira49 <jpedrostonemoreira@gmail.com>
2022-08-16 15:37:25 -0300

    Add config page

---
+++ src/Views/templates/config.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>src/views/templates/css/styles.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>New Daw</title>
+</head>
+<body>
+<div class="form-style-6">
+	<h1>Configurations:</h1>
+	<form>
+		<p><a href="<?php echo INCLUDE_PATH; ?>configchangepass">Change password</a></p>
+	</form>
+</div>
+</body>
+</html>
