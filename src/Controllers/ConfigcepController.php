bed29f223fdd98f28ba1a8ef79a95a3d368c7d2c
pedromoreira49 <jpedrostonemoreira@gmail.com>
2022-08-30 16:57:45 -0300

    Created controller for config cep

---
+++ src/Controllers/ConfigcepController.php
@@ -0,0 +1,12 @@
+<?php
+
+    namespace src\Controllers;
+
+    class ConfigcepController{
+        public static function index(){
+            \src\Views\MainView::render('configcep');
+        }
+    }
+
+
+?>
